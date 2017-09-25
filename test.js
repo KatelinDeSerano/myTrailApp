5c2c4ac505319444afcc93aecfb938384293bd9f
Katelin DeSerano <katelindeserano@Katelins-MacBook-Pro.local>
2017-09-24 23:17:49 -0500

    Passed Latitude and Longitude to get data from the Trails API

--- test.js
+++ test.js
@@ -30,4 +30,28 @@ $("#cities").change(function () {
     let longitude = $(this).find(':selected').data('longitude');
     console.log(latitude);
     console.log(longitude);
+    getDataFromTrails(latitude,longitude,displayTrailSearchData);
 });
+
+function getDataFromTrails(latitude, longitude, callback) {
+    const settings = {
+      url: 'https://trailapi-trailapi.p.mashape.com/',
+      headers: {
+        "X-Mashape-Key":'Jiv2iCwuWKmshf598IFTnsJC7Va7p15GAbhjsnMVlaeqLJbqru'
+      },
+      data: {
+        lat: latitude,
+        lon: longitude,
+        radius: 100,
+        limit: 5,
+      },
+      dataType: 'json',
+      type: 'GET',
+      success: callback
+    };
+    $.ajax(settings);
+  }
+  
+function displayTrailSearchData(data){
+    console.log(data);
+}
