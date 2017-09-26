51300b6ff066e19d18f4bb13e44ca8e4ff3eb64d
Katelin DeSerano <katelindeserano@Katelins-MacBook-Pro.local>
2017-09-25 22:14:10 -0500

    Added markers on map for each result and info window

--- test.js
+++ test.js
@@ -1,15 +1,16 @@
+// get data for drowdown menus from Battuta API
 let countryCode = "us";
 let BATTUTA_KEY = "1b568dfcb5d096e0c78327fb27f6e590";
 url = "https://battuta.medunes.net/api/region/" + countryCode + "/all/?key=" + BATTUTA_KEY + "&callback=?";
 
 $.getJSON(url, function (region) {
-    
     var state = region;
     var option = '';
     for (var i = 0; i < state.length; i++) {
         option += '<option value="' + state[i].region + '">' + state[i].region + '</option>';
     }
     $('#items').append(option);
+     
 });
 
 $('#items').on('change', function () {
@@ -40,7 +41,7 @@ function getDataFromTrails(latitude, longitude, callback) {
             lat: latitude,
             lon: longitude,
             radius: 100,
-            limit: 5,
+            limit: 10,
         },
         dataType: 'json',
         type: 'GET',
@@ -57,29 +58,27 @@ function initialize() {
     });
 }
 
-// function displayTrailSearchData(data) {
-//     console.log(data.places);
-//     let arr = data.places
-//     var bounds = new google.maps.LatLngBounds();
-//     for (let i = 0; i < arr.length; i++) {
-//         position = new google.maps.LatLng(arr[i].lat, arr[i].lon);
-//         marker = new google.maps.Marker({
-//             position: position,
-//             map: map
-//         });
-//         bounds.extend(position)
-//         google.maps.event.addListener(marker, 'click', (function (marker, i) {
-//             return function () {
-//                 infowindow.setContent('<h3>' + arr[i].name + '</h3><h4>' + arr[i].directions + '</h4>');
-//                 infowindow.open(map, marker);
-//             }
-//         })(marker, i));
+function displayTrailSearchData(data) {
+    console.log(data.places);
+    let arr = data.places
+    var bounds = new google.maps.LatLngBounds();
+    for (let i = 0; i < arr.length; i++) {
+        position = new google.maps.LatLng(arr[i].lat, arr[i].lon);
+        marker = new google.maps.Marker({
+            position: position,
+            map: map
+        });
+        bounds.extend(position)
+        google.maps.event.addListener(marker, 'click', (function (marker, i) {
+            return function () {
+                infowindow.setContent('<h3>' + arr[i].name + '</h3><h4>' + arr[i].directions + '</h4>');
+                infowindow.open(map, marker);
+            }
+        })(marker, i));
+    }
 
-//     }
-//     map.fitBounds(bounds);
-
-//     var infowindow = new google.maps.InfoWindow();
-
-//     var marker, i;
-// }
+    map.fitBounds(bounds);
+    var infowindow = new google.maps.InfoWindow();
+    var marker, i;
+}
 
