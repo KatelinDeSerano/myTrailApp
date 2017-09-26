3009e26f4493dab2627a91167113e256ee666c95
Katelin DeSerano <katelindeserano@Katelins-MacBook-Pro.local>
2017-09-26 02:51:03 -0500

    Added a side navbar and styling

--- test.js
+++ test.js
@@ -1,33 +1,44 @@
 // get data for drowdown menus from Battuta API
-let countryCode = "us";
-let BATTUTA_KEY = "1b568dfcb5d096e0c78327fb27f6e590";
+var countryCode = "us";
+var BATTUTA_KEY = "1b568dfcb5d096e0c78327fb27f6e590";
 url = "https://battuta.medunes.net/api/region/" + countryCode + "/all/?key=" + BATTUTA_KEY + "&callback=?";
 
-$.getJSON(url, function (region) {
-    var state = region;
+
+
+$(".button-collapse").sideNav();
+$.getJSON(url, function (states) {
     var option = '';
-    for (var i = 0; i < state.length; i++) {
-        option += '<option value="' + state[i].region + '">' + state[i].region + '</option>';
+    for (var i = 0; i < states.length; i++) {
+        option += '<option value="' + states[i].region + '">' + states[i].region + '</option>';
     }
     $('#items').append(option);
-     
+
+    $(document).ready(function() {
+        $('select').material_select();
+      });
 });
 
+
+
 $('#items').on('change', function () {
-    let region = this.value;
+    var region = this.value;
     url = "https://battuta.medunes.net/api/city/" + countryCode + "/search/?region=" + region + "&key=" + BATTUTA_KEY + "&callback=?";
     $.getJSON(url, function (city) {
         var option = '';
+        $("#cities option").remove();
         for (var i = 0; i < city.length; i++) {
             option += '<option data-latitude="' + city[i].latitude + '" data-longitude="' + city[i].longitude + '">' + city[i].city + '</option>';
         }
         $('#cities').append(option);
+        $(document).ready(function() {
+            $('select').material_select();
+          });
     });
 })
 
-$("#cities").change(function () {
-    let latitude = $(this).find(':selected').data('latitude');
-    let longitude = $(this).find(':selected').data('longitude');
+$("#cities").on('change', function () {
+    var latitude = $(this).find(':selected').data('latitude');
+    var longitude = $(this).find(':selected').data('longitude');
     getDataFromTrails(latitude, longitude, displayTrailSearchData);
 });
 
@@ -41,7 +52,7 @@ function getDataFromTrails(latitude, longitude, callback) {
             lat: latitude,
             lon: longitude,
             radius: 100,
-            limit: 10,
+            limit: 2,
         },
         dataType: 'json',
         type: 'GET',
@@ -51,6 +62,7 @@ function getDataFromTrails(latitude, longitude, callback) {
 }
 
 var map;
+var markers = [];
 function initialize() {
     map = new google.maps.Map(document.getElementById('map'), {
         center: { lat: -34.397, lng: 150.644 },
@@ -59,26 +71,34 @@ function initialize() {
 }
 
 function displayTrailSearchData(data) {
-    console.log(data.places);
-    let arr = data.places
+    var arr = data.places
+   
     var bounds = new google.maps.LatLngBounds();
-    for (let i = 0; i < arr.length; i++) {
+    for (var i = 0; i < arr.length; i++) {
         position = new google.maps.LatLng(arr[i].lat, arr[i].lon);
         marker = new google.maps.Marker({
             position: position,
             map: map
         });
-        bounds.extend(position)
+        markers.push(marker);
+        bounds.extend(position);
         google.maps.event.addListener(marker, 'click', (function (marker, i) {
             return function () {
                 infowindow.setContent('<h3>' + arr[i].name + '</h3><h4>' + arr[i].directions + '</h4>');
                 infowindow.open(map, marker);
             }
         })(marker, i));
+        
     }
+    function deleteMarkers() {
+        clearMarkers();
+        markers = [];
+      }
 
     map.fitBounds(bounds);
     var infowindow = new google.maps.InfoWindow();
     var marker, i;
+
 }
+
 
